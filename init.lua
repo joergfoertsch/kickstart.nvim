190b9fe7155227f0b7a2ffae92ed33a72c7d9fe8
Jörg Förtsch <j.foertsch@uni-wuppertal.de>
2026-09-23 12:24:41 +0200

    Update

--- init.lua
+++ init.lua
@@ -165,8 +165,8 @@ vim.opt.confirm = true
 vim.opt.wrap = true
 
 -- Tab-Setup
-vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- insert 4 spaces for a tab
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -221,7 +221,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
@@ -368,7 +368,8 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    -- branch = '0.1.x',
+    branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -985,22 +986,44 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    branch = 'main',
+    -- main = 'nvim-treesitter', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    config = function()
+      require('nvim-treesitter')
+        .install({
+          'bash',
+          'c',
+          'diff',
+          'html',
+          'lua',
+          'luadoc',
+          'markdown',
+          'markdown_inline',
+          'query',
+          'vim',
+          'vimdoc',
+          'javascript',
+          'latex',
+          'rust',
+        })
+        :wait(300000)
+    end,
+    -- opts = {
+    --   ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+    --   -- Autoinstall languages that are not installed
+    --   auto_install = true,
+    --   highlight = {
+    --     enable = true,
+    --     -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+    --     --  If you are experiencing weird indenting issues, add the language to
+    --     --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+    --     additional_vim_regex_highlighting = { 'ruby' },
+    --   },
+    --   indent = { enable = true, disable = { 'ruby' } },
+    -- },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
---
+++ lua/custom/plugins/neogen.lua
@@ -0,0 +1,10 @@
+-- neogen
+-- https://github.com/danymat/neogen
+--
+
+return {
+  'danymat/neogen',
+  config = true,
+  -- Uncomment next line if you want to follow only stable versions
+  -- version = "*"
+}
